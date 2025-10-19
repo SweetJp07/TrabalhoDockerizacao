7123a951a8f15f46d504512b608fff55362bda19
João Pedro Mendes Pereira <joaopedromendespereira1@gmail.com>
2025-10-19 19:05:31 -0300

    Versão dockerizada do trabalho de sistemas distribuidos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, //Tive que adicionar o server, pois meu notbook tava abrindo na porta 5174
+    host: true
+  }
+
 })
